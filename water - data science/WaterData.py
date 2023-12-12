665130f0c86e4bb8408731feeaa719c1dad04165
aSimpson02 <149061939+aSimpson02@users.noreply.github.com>
2023-12-12 08:28:45 +0000

    Update WaterData.py

--- water - data science/WaterData.py
+++ water - data science/WaterData.py
@@ -30,12 +30,12 @@ dataC = pd.read_csv('gdp.csv')
 data3 = dataC[['Country Name', '2020 [YR2020]', '2013 [YR2013]']]
 df = data3.rename(columns={'Country Name': 'country'})
 
-dataD = pd.read_csv('world_country_and_usa_states_latitude_and_longitude_values2.csv')
-data4 = dataD[['latitude', 'longitude', 'country']]
+#dataD = pd.read_csv('world_country_and_usa_states_latitude_and_longitude_values2.csv')
+#data4 = dataD[['latitude', 'longitude', 'country']]
 
 
 #merging the data sets together:
 #data= data1.merge(df)
-Finaldata = pd.merge(data1, data2, on='country').merge(df, on='country')
+Finaldata = pd.merge(data, data3, on='country').merge(df, on='country')
 Finaldata.to_csv('data.csv', index=False)
 print(Finaldata.index)
